c64c760f71ccfee4671530bb8aecd08b6a9b5a97
timerke <timerke@mail.ru>
2022-11-17 11:22:40 +0300

    Returned setup.py version to install module from git

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup()
+setup(name="laiarturs-ros_api",
+      version="1.0.0",
+      description="Connect to and use API interface of MikroTik RouterOS",
+      long_description=open("README.md", "r", encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
+      url="https://github.com/timerke/RouterOS_API",
+      author="",
+      author_email="",
+      packages=find_packages(),
+      install_requires=[],
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Development Status :: 4 - Beta",
+                   "Topic :: System :: Networking"],
+      python_requires=">=3.4",
+      )
